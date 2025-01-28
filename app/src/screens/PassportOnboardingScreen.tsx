d5c6d5f5a5b101c26f2f987af86b85944bfcc5db
Leszek Stachowski <leszek.stachowski@clabs.co>
2025-01-28 15:23:58 +0100

    WIP: passport onboarding screen

--- app/src/screens/PassportOnboardingScreen.tsx
+++ app/src/screens/PassportOnboardingScreen.tsx
@@ -2,35 +2,50 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Image, YStack } from 'tamagui';
 
-import Logo from '../images/logo.png';
-import { ExpandableBottomLayout } from '../layouts/ExpandableBottomLayout';
+import Passport from '../images/passport.png';
 import { PrimaryButton } from '../components/buttons/PrimaryButton';
-import { slate50, slate100, slate700, slate500 } from '../utils/colors';
+import { slate400, slate500, black } from '../utils/colors';
+import { SecondaryButton } from '../components/buttons/SecondaryButton';
+import { ExpandableBottomLayout } from '../layouts/ExpandableBottomLayout';
 import { useNavigation } from '@react-navigation/native';
 
 interface PassportOnboardingScreenProps {}
 
-const PassportOnboardingScreen: React.FC<
-  PassportOnboardingScreenProps
-> = ({}) => {
+const PassportOnboardingScreen: React.FC<PassportOnboardingScreenProps> = ({}) => {
   const navigation = useNavigation();
 
   return (
     <ExpandableBottomLayout.Layout>
       <ExpandableBottomLayout.TopSection>
-        <Image src={Logo} />
+        <Image
+          resizeMethod="auto"
+          source={{ uri: Passport }}
+          style={{
+            width: '90%',
+            height: '90%',
+            aspectRatio: 0.69,
+          }}
+        />
       </ExpandableBottomLayout.TopSection>
       <ExpandableBottomLayout.BottomSection>
-        <YStack gap="$2.5">
-          <Text style={styles.subheader}>
+        <YStack alignItems='center' gap="$2.5">
+          <Text style={styles.title}>Scan your passport</Text>
+          <Text style={styles.description}>
             Open your passport to the first page to scan it.
           </Text>
-          <PrimaryButton
-            onPress={() => navigation.navigate('TODO: open camera')}
+          <Text style={styles.additional}>
+            Self ID will not capture an image of your passport. Our system is
+            only reading the fields.
+          </Text>
+          <PrimaryButton onPress={
+            // @ts-ignore TODO
+            () => navigation.navigate('Camera')}
           >
             Open Camera
           </PrimaryButton>
-          <PrimaryButton onPress={navigation.goBack}>Cancel</PrimaryButton>
+          <SecondaryButton onPress={() => navigation.navigate('Home')}>
+            Cancel
+          </SecondaryButton>
         </YStack>
       </ExpandableBottomLayout.BottomSection>
     </ExpandableBottomLayout.Layout>
@@ -40,27 +55,25 @@ const PassportOnboardingScreen: React.FC<
 export default PassportOnboardingScreen;
 
 const styles = StyleSheet.create({
-  subheader: {
-    color: slate700,
-    // fontWeight: '500',
-    fontSize: 20,
-    lineHeight: 26,
-    textAlign: 'center',
+  title: {
+    fontSize: 28,
+    fontWeight: '400',
+    lineHeight: 35,
+    color: black,
   },
-  link: {
-    textDecorationLine: 'underline',
-  },
-  notice: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    paddingRight: 20,
-    backgroundColor: slate50,
-    borderColor: slate100,
-    borderWidth: 1,
-    borderStyle: 'solid',
+  description: {
+    textAlignVertical: 'center',
     color: slate500,
+    fontWeight: '500',
+    fontSize: 18,
+    lineHeight: 23,
     textAlign: 'center',
+  },
+  additional: {
+    fontSize: 14,
+    fontWeight: '500',
     lineHeight: 18,
+    textAlign: 'center',    
+    color: slate400,
   },
 });
