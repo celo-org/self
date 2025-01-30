7faec4e80e3e4910045d55aadfe6f977f1b7f05e
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-01-30 09:33:35 +0100

    Refactor Typography to be act like normal text component
    add LargeTitle

--- app/src/components/typography/LargeTitle.tsx
+++ app/src/components/typography/LargeTitle.tsx
@@ -3,17 +3,19 @@ import { StyleSheet, Text, TextProps } from 'react-native';
 import { black } from '../../utils/colors';
 
 interface TitleProps extends TextProps {}
-
-const Title = ({ children, style, ...props }: TitleProps) => {
+/*
+ * Used for Prominent Top Page Titles
+ */
+const LargeTitle = ({ children, style, ...props }: TitleProps) => {
   return <Text {...props} style={[styles.title, style]}>{children}</Text>;
 };
 
-export default Title;
+export default LargeTitle;
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 28,
-    lineHeight: 35,
+    fontSize: 38,
+    lineHeight: 47,
     color: black,
     fontFamily: 'Advercase-Regular',
   },
