fec8b009b1e33ef04d92904931e29ff10951cbb3
Justin Hernandez <transphorm@gmail.com>
2025-02-10 16:18:55 -0600

    fix splash check

--- app/src/screens/SplashScreen.tsx
+++ app/src/screens/SplashScreen.tsx
@@ -11,22 +11,14 @@ import { impactLight } from '../utils/haptic';
 
 const SplashScreen: React.FC = ({}) => {
   const navigation = useNavigation();
-  const { userLoaded, passportData } = useUserStore();
-
-  const redirect = useCallback(() => {
-    if (passportData) {
-      navigation.navigate('Home');
-    } else {
-      navigation.navigate('Launch');
-    }
-  }, [passportData, userLoaded]);
+  const { userLoaded } = useUserStore();
 
   const handleAnimationFinish = useCallback(() => {
     setTimeout(() => {
       impactLight();
-      redirect();
-    }, 750);
-  }, [redirect]);
+      navigation.navigate('Launch');
+    }, 1000);
+  }, [userLoaded]);
 
   return (
     <LottieView
