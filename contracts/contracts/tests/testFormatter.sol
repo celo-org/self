71b59eb50cf37b96e997ed0ead8bb517ceacceab
motemotech <i.am.nicoshark@gmail.com>
2025-02-11 14:59:18 +0900

    fix to named imports

--- contracts/contracts/tests/testFormatter.sol
+++ contracts/contracts/tests/testFormatter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import "../libraries/Formatter.sol";
+import {Formatter} from "../libraries/Formatter.sol";
 
 contract TestFormatter {
     // tested
