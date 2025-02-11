28c541b143faff6e6329dee387a09f95b87a4640
Justin Hernandez <transphorm@gmail.com>
2025-02-10 18:30:32 -0600

    update launch screen design

--- app/src/screens/LaunchScreen.tsx
+++ app/src/screens/LaunchScreen.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { Anchor, Text, YStack } from 'tamagui';
+import { Anchor, Text, XStack, YStack } from 'tamagui';
 
 import { PrimaryButton } from '../components/buttons/PrimaryButton';
 import useHapticNavigation from '../hooks/useHapticNavigation';
 import GetStartedCard from '../images/get-started-card.svg';
 import Logo from '../images/logo.svg';
 import { ExpandableBottomLayout } from '../layouts/ExpandableBottomLayout';
-import { slate50, slate100, slate500, slate700 } from '../utils/colors';
+import { slate50, slate100, slate500, slate700, white } from '../utils/colors';
 
 const LaunchScreen: React.FC = () => {
   const onStartPress = useHapticNavigation('Start');
@@ -16,8 +16,27 @@ const LaunchScreen: React.FC = () => {
   return (
     <ExpandableBottomLayout.Layout>
       <ExpandableBottomLayout.TopSection>
-        <GetStartedCard />
-        <Logo />
+        <YStack
+          flex={1}
+          justifyContent="flex-start"
+          paddingVertical="$2"
+          gap="$4"
+        >
+          <View style={styles.cardContainer}>
+            <GetStartedCard style={styles.card} />
+          </View>
+          <YStack flex={1} justifyContent="flex-end">
+            <XStack
+              marginBottom="$10"
+              alignItems="center"
+              justifyContent="center"
+              gap="$4"
+            >
+              <Logo style={styles.logo} />
+              <Text style={styles.selfText}>Self</Text>
+            </XStack>
+          </YStack>
+        </YStack>
       </ExpandableBottomLayout.TopSection>
       <ExpandableBottomLayout.BottomSection>
         <YStack gap="$2.5">
@@ -37,7 +56,9 @@ const LaunchScreen: React.FC = () => {
             </Anchor>
             &nbsp;of Self provided by Self Inc.
           </Text>
-          <PrimaryButton onPress={onStartPress}>Get Started</PrimaryButton>
+          <PrimaryButton style={styles.button} onPress={onStartPress}>
+            Get Started
+          </PrimaryButton>
         </YStack>
       </ExpandableBottomLayout.BottomSection>
     </ExpandableBottomLayout.Layout>
@@ -70,5 +91,27 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     lineHeight: 18,
     fontWeight: '500',
+  },
+  cardContainer: {
+    width: '100%',
+    alignItems: 'center',
+    paddingTop: 0,
+  },
+  selfText: {
+    fontFamily: 'Advercase-Regular',
+    fontSize: 36,
+    fontWeight: '500',
+    color: white,
+  },
+  button: {
+    marginBottom: '15%',
+  },
+  card: {
+    width: '100%',
+    height: '100%',
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
   },
 });
