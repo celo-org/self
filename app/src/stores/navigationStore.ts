f3bddd3fb39df67b88063ae9308a52bd47f21ecc
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-02-14 14:53:44 +0100

    lint

--- app/src/stores/navigationStore.ts
+++ app/src/stores/navigationStore.ts
@@ -3,21 +3,19 @@ import { create } from 'zustand';
 
 import { segmentClient } from '../../App';
 
-
 interface NavigationState {
   toast: ReturnType<typeof useToastController>;
   trackEvent: (eventName: string, properties?: Record<string, any>) => void;
 }
 
 const useNavigationStore = create<NavigationState>(() => ({
-
   toast: null as unknown as ReturnType<typeof useToastController>,
 
   trackEvent: (eventName: string, properties?: Record<string, any>) => {
     if (segmentClient) {
       segmentClient.track(eventName, properties);
     }
-  }
+  },
 }));
 
 export default useNavigationStore;
