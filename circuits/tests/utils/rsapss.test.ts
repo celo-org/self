9f3d85ae1cee9de6c73718da3c4ce2ae312dde2b
seshanthS <seshanth@protonmail.com>
2025-02-18 01:13:44 +0530

    lint

--- circuits/tests/utils/rsapss.test.ts
+++ circuits/tests/utils/rsapss.test.ts
@@ -1,7 +1,10 @@
 import { wasm as wasmTester } from 'circom_tester';
 import { describe, it } from 'mocha';
 import path from 'path';
-import { generateMalleableRsaPssInputs, generateMockRsaPssInputs } from './generateMockInputsRsaPss';
+import {
+  generateMalleableRsaPssInputs,
+  generateMockRsaPssInputs,
+} from './generateMockInputsRsaPss';
 import { expect } from 'chai';
 import { fullAlgorithms, sigAlgs, AdditionalCases } from './testcase/rsapss';
 
@@ -27,16 +30,16 @@ describe('VerifyRsapss Circuit Test', function () {
             include: ['node_modules', './node_modules/@zk-kit/binary-merkle-root.circom/src'],
           }
         );
-  
+
         try {
           const witness = await circuit.calculateWitness({
             signature,
             modulus,
             message,
           });
-        
+
           // Check constraints
-          await circuit.checkConstraints(witness);        
+          await circuit.checkConstraints(witness);
         } catch (error) {
           if (additionalCase.shouldFail) {
             expect(error.message).to.include('Assert Failed');
@@ -135,43 +138,15 @@ describe('VerifyRsapss Circuit Test', function () {
       }
     });
 
-  it('Should reject signatures greater than or equal to modulus', async function () {
-    const { signature, modulus, message, n, k } = generateMockRsaPssInputs(
-      algorithm.algo,
-      algorithm.saltLength
-    );
-  
-    const largeSignature = [...signature];
-    largeSignature[k-1] = String(BigInt(modulus[k-1]) + 1n);
-  
-    const circuit = await wasmTester(
-      path.join(
-        __dirname,
-        `../../circuits/tests/utils/rsapss/test_${algorithm.algo}_${algorithm.saltLength}.circom`
-      ),
-      {
-        include: ['node_modules', './node_modules/@zk-kit/binary-merkle-root.circom/src'],
-      }
-    );
-  
-    try {
-      await circuit.calculateWitness({
-        signature: largeSignature,
-        modulus,
-        message,
-      });
-      throw new Error('Circuit accepted signature >= modulus');
-    } catch (error) {
-      expect(error.message).to.include('Assert Failed');
-    }
-    });
-    
-    it('Should reject malleable signatures (signature + modulus)', async function () {
-      const { signature, modulus, message } = generateMalleableRsaPssInputs(
+    it('Should reject signatures greater than or equal to modulus', async function () {
+      const { signature, modulus, message, n, k } = generateMockRsaPssInputs(
         algorithm.algo,
         algorithm.saltLength
       );
-    
+
+      const largeSignature = [...signature];
+      largeSignature[k - 1] = String(BigInt(modulus[k - 1]) + 1n);
+
       const circuit = await wasmTester(
         path.join(
           __dirname,
@@ -181,7 +156,35 @@ describe('VerifyRsapss Circuit Test', function () {
           include: ['node_modules', './node_modules/@zk-kit/binary-merkle-root.circom/src'],
         }
       );
-    
+
+      try {
+        await circuit.calculateWitness({
+          signature: largeSignature,
+          modulus,
+          message,
+        });
+        throw new Error('Circuit accepted signature >= modulus');
+      } catch (error) {
+        expect(error.message).to.include('Assert Failed');
+      }
+    });
+
+    it('Should reject malleable signatures (signature + modulus)', async function () {
+      const { signature, modulus, message } = generateMalleableRsaPssInputs(
+        algorithm.algo,
+        algorithm.saltLength
+      );
+
+      const circuit = await wasmTester(
+        path.join(
+          __dirname,
+          `../../circuits/tests/utils/rsapss/test_${algorithm.algo}_${algorithm.saltLength}.circom`
+        ),
+        {
+          include: ['node_modules', './node_modules/@zk-kit/binary-merkle-root.circom/src'],
+        }
+      );
+
       try {
         await circuit.calculateWitness({
           signature,
@@ -202,7 +205,7 @@ describe('VerifyRsapss Circuit Test', function () {
 
       let overflowSignature = [...signature];
       overflowSignature[0] = String(BigInt(2) ** BigInt(122));
-    
+
       const circuit = await wasmTester(
         path.join(
           __dirname,
@@ -212,7 +215,7 @@ describe('VerifyRsapss Circuit Test', function () {
           include: ['node_modules', './node_modules/@zk-kit/binary-merkle-root.circom/src'],
         }
       );
-    
+
       try {
         await circuit.calculateWitness({
           signature: overflowSignature,
