bd9d4111e91120dc4185c6558c73304ff5f4e647
Justin Hernandez <transphorm@gmail.com>
2025-02-18 13:15:26 -0600

    recreate params to remove functions

--- app/src/stores/navigationStore.ts
+++ app/src/stores/navigationStore.ts
@@ -22,10 +22,14 @@ const useNavigationStore = create<NavigationState>(() => ({
       return segmentClient.track(eventName);
     }
 
-    for (const key of Object.keys(properties.params)) {
-      if (typeof properties.params[key] === 'function') {
-        delete properties.params[key];
+    if (properties.params) {
+      const newParams = {};
+      for (const key of Object.keys(properties.params)) {
+        if (typeof properties.params[key] !== 'function') {
+          (newParams as Record<string, any>)[key] = properties.params[key];
+        }
       }
+      properties.params = newParams;
     }
 
     segmentClient.track(eventName, properties);
