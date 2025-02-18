adeeb252af0869ac0c92880e0f4cbdeb62cd6ff8
Justin Hernandez <transphorm@gmail.com>
2025-02-18 12:44:21 -0600

    don't send serialized functions to segment

--- app/src/stores/navigationStore.ts
+++ app/src/stores/navigationStore.ts
@@ -14,9 +14,17 @@ const useNavigationStore = create<NavigationState>(() => ({
   toast: null as unknown as ReturnType<typeof useToastController>,
 
   trackEvent: (eventName: string, properties?: Record<string, any>) => {
-    if (segmentClient) {
-      segmentClient.track(eventName, properties);
+    if (!segmentClient || !properties) {
+      return segmentClient?.track(eventName);
     }
+
+    for (const key of Object.keys(properties)) {
+      if (typeof properties[key] === 'function') {
+        delete properties[key];
+      }
+    }
+
+    segmentClient.track(eventName, properties);
   },
 }));
 
