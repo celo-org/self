bcd2a8d88d30e8640eb4055e76eae478f3e930e5
Justin Hernandez <transphorm@gmail.com>
2025-02-20 09:14:51 -0600

    change enable to manage

--- app/src/screens/AccountFlow/SaveRecoveryPhraseScreen.tsx
+++ app/src/screens/AccountFlow/SaveRecoveryPhraseScreen.tsx
@@ -80,7 +80,7 @@ const SaveRecoveryPhraseScreen: React.FC<
           You can reveal your recovery phrase in settings.
         </Caption>
         <PrimaryButton onPress={onCloudBackupPress}>
-          Enable {STORAGE_NAME} backups
+          Manage {STORAGE_NAME} backups
         </PrimaryButton>
         <SecondaryButton onPress={onSkipPress}>
           {userHasSeenMnemonic ? 'Continue' : 'Skip making a backup'}
