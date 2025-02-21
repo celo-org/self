112da829fff127ea9622b9ebe018cb55b52102ad
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-02-21 17:59:44 +0100

    fix: limt

--- app/src/screens/Settings/ShowRecoveryPhraseScreen.tsx
+++ app/src/screens/Settings/ShowRecoveryPhraseScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 
 import Mnemonic from '../../components/Mnemonic';
 import Description from '../../components/typography/Description';
